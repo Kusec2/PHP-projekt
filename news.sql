5d590ce1bc0408154dc51f3a412d6fad686dd367
Kusec2 <mkusec1@vvg.hr>
2025-01-07 23:37:45 +0100

    Cetvrti commit

---
+++ news.sql
@@ -0,0 +1,11 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+CREATE TABLE `news` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `picture` varchar(255) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `archive` enum('N','Y') NOT NULL DEFAULT 'N'
+);
